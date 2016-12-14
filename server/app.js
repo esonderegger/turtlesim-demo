ef5f0334c180caa1912e287df145b99c0b2b6120
Evan Sonderegger <evan@rpy.xyz>
2016-12-14 00:01:44 -0500

    enable drawing of arbitrary paths with multiple turtles

--- server/app.js
+++ server/app.js
@@ -5,38 +5,81 @@ const io = require('socket.io')(http);
 const rosnodejs = require('rosnodejs');
 const SimulatedTurtle = require('./simulated-turtle.js');
 
-let turtle1 = null;
+let turtles = [];
+let killTurtle = null;
+let drawPaths = null;
 
 rosnodejs.initNode('/my_node', {onTheFly: true}).then((rosNode) => {
-  turtle1 = new SimulatedTurtle(rosNode, 0, 0, 0);
-  turtle1.on('pose', (msg) => {
-    io.emit('turtle1', msg);
+  killTurtle = (turtleName, callback) => {
+    const killMessage = rosnodejs.require('turtlesim').srv.Kill;
+    const killRequest = new killMessage.Request({name: turtleName});
+    let killClient = rosNode.serviceClient('/kill', 'turtlesim/Kill');
+    rosNode.waitForService(killClient.getService(), 2000)
+      .then((available) => {
+        if (available) {
+          killClient.call(killRequest, (resp) => {
+            callback();
+          });
+        } else {
+          console.log('Service not available');
+        }
+      });
+  };
+  drawPaths = (paths, callback) => {
+    for (let a = 0; a < turtles.length; a++) {
+      killTurtle(turtles[a].name);
+      turtles[a].exists = false;
+      delete turtles[a].turtle;
+    }
+    turtles = [];
+    setTimeout(() => {
+      io.emit('clear all paths', 'now');
+    }, 100);
+    for (let b = 0; b < paths.length; b++) {
+      let newTurtleName = 'turtle' + (b + 1);
+      let newTurtle = new SimulatedTurtle(
+        rosNode,
+        paths[b][0][0],
+        paths[b][0][1],
+        0,
+        newTurtleName
+      );
+      let newTurtleObj = {
+        name: newTurtleName,
+        turtle: newTurtle,
+        exists: true,
+      };
+      newTurtle.on('pose', (msg) => {
+        if (newTurtleObj.exists) {
+          io.emit(newTurtleName, msg);
+        }
+      });
+      newTurtle.drawPath(paths[b], () => {
+        console.log('path segment complete.');
+      });
+      turtles.push(newTurtle);
+    }
+  };
+  killTurtle('turtle1');
+  let originalTurtle = {
+    name: 'turtle1',
+    turtle: new SimulatedTurtle(rosNode, 5.5, 5.5, 0, 'turtle1'),
+    exists: true,
+  };
+  originalTurtle.turtle.on('pose', (msg) => {
+    if (originalTurtle.exists) {
+      io.emit(originalTurtle.name, msg);
+    }
   });
+  turtles.push(originalTurtle);
 });
 
 app.use(express.static('client/public'));
 
 io.on('connection', function(socket) {
-  socket.on('drawstar', function() {
-    const starpath = [
-      [4, 0],
-      [3, 3],
-      [0, 3],
-      [2.5, 5],
-      [1.5, 8],
-      [4, 6],
-      [6.5, 8],
-      [5.5, 5],
-      [8, 3],
-      [5, 3],
-      [4, 0],
-    ];
-    const scaledPath = starpath.map((point) => {
-      return [(point[0] / 8 * 10) + 0.5, 10.5 - (point[1] / 8 * 10)];
-    });
-    turtle1.drawPath(scaledPath, () => {
-      console.log('all done!');
-      io.emit('pathmessage', 'star complete');
+  socket.on('draw paths', function(paths) {
+    drawPaths(paths, () => {
+      io.emit('path message', 'path complete');
     });
   });
 });
